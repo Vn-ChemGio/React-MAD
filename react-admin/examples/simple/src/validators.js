ad63ffc20ccf152706809559c2f02b5329c25993
vn.chemgio@yahoo.com <vnn126973>
2019-11-18 15:51:09 +0700

    reset project

--- react-admin/examples/simple/src/validators.js
+++
@@ -1,7 +0,0 @@
-import {
-    required as createRequiredValidator,
-    number as createNumberValidator,
-} from 'react-admin1';
-
-export const required = createRequiredValidator();
-export const number = createNumberValidator();
