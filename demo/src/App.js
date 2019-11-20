9f484e11d282b16a74374bc426b57c0ff0be02e9
WindBlade <vn.chemgio@yahoo.com>
2019-11-20 23:21:51 +0700

    upgrade version packet

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,63 +1,79 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Admin, Resource } from 'react-admin';
+import polyglotI18nProvider from 'ra-i18n-polyglot';
+import jsonServerProvider from 'ra-data-json-server';
+
 
 import './App.css';
 
 import authProvider from './authProvider';
-import { Login, Layout } from './layout';
-import { Dashboard } from './dashboard';
+import themeReducer from './themeReducer';
 
+import { Dashboard } from './pages/dashboard';
+import Login  from './pages/Login';
+import customRoutes from './routes';
+import englishMessages from './i18n/en';
+
+import UserList from './users'
 
 import dataProviderFactory from './dataProvider';
 import fakeServerFactory from './fakeServer';
-import visitors from "../src/visitors";
 
 
 
-class App extends Component {
-    state = { dataProvider: null };
 
-    async componentWillMount() {
-        this.restoreFetch = await fakeServerFactory(
-            process.env.REACT_APP_DATA_PROVIDER
-        );
-
-        const dataProvider = await dataProviderFactory(
-            process.env.REACT_APP_DATA_PROVIDER
-        );
-
-        this.setState({ dataProvider });
+const i18nProvider = polyglotI18nProvider(locale => {
+    if (locale === 'fr') {
+        return import('./i18n/fr').then(messages => messages.default);
     }
 
-    componentWillUnmount() {
-        this.restoreFetch();
-    }
+    // Always fallback on english
+    return englishMessages;
+}, 'en');
 
-    render() {
-        const { dataProvider } = this.state;
+const App = () => {
+    let [dataProvider, setDataProvider] = useState(null);
+    dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+    // useEffect(() => {
+    //     let restoreFetch;
+    //
+    //     const fetchDataProvider = async () => {
+    //         restoreFetch = await fakeServerFactory(
+    //             process.env.REACT_APP_DATA_PROVIDER
+    //         );
+    //
+    //         setDataProvider(
+    //             await dataProviderFactory(process.env.REACT_APP_DATA_PROVIDER)
+    //         );
+    //     };
+    //
+    //     fetchDataProvider();
+    //
+    //     return restoreFetch;
+    // }, []);
 
-        if (!dataProvider) {
-            return (
-                <div className="loader-container">
-                    <div className="loader">Loading...</div>
-                </div>
-            );
-        }
-
+    if (!dataProvider) {
         return (
-
-            <Admin
-                title=""
-                dataProvider={dataProvider}
-                authProvider={authProvider}
-                dashboard={Dashboard}
-                loginPage={Login}
-            >
-
-                <Resource name="customers" {...visitors} />
-            </Admin>
+            <div className="loader-container">
+                <div className="loader">Loading...</div>
+            </div>
         );
     }
-}
+
+    return (
+        <Admin
+            title=""
+            dataProvider={dataProvider}
+
+            authProvider={authProvider}
+            dashboard={Dashboard}
+            loginPage={Login}
+
+            i18nProvider={i18nProvider}
+        >
+            <Resource name="posts" list={UserList} />
+        </Admin>
+    );
+};
 
 export default App;
--- demo/src/authProvider.js
+++ demo/src/authProvider.js
@@ -1,9 +1,9 @@
-import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_CHECK, AUTH_ERROR } from 'react-admin';
-
+import {AUTH_LOGIN, AUTH_LOGOUT, AUTH_CHECK, AUTH_ERROR} from 'react-admin';
+/*
 export default (type, params) => {
     if (type === AUTH_LOGIN) {
         const { username } = params;
-      /*  localStorage.setItem('username', username);*/
+      /!*  localStorage.setItem('username', username);*!/
         // accept all username/password combinations
         return Promise.reject('Unkown method');
     }
@@ -20,4 +20,39 @@ export default (type, params) => {
             : Promise.reject();
     }
     return Promise.reject('Unkown method');
+};*/
+
+ const authProvider = {
+    login: ({username, password}) => {
+        const request = new Request('http://gmt.glwwzgfb.com:5400/api/login', {
+            method: 'POST',
+            body: JSON.stringify({username, password}),
+            headers: new Headers({'Content-Type': 'application/json'}),
+        });
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(({userId}) => {
+                localStorage.setItem('token', userId);
+            });
+    },
+    logout: params => Promise.resolve(),
+    checkAuth: params => localStorage.getItem('token')
+        ? Promise.resolve()
+        : Promise.reject(),
+    checkError: error => {
+        const status = error.status;
+        if (status === 401 || status === 403) {
+            localStorage.removeItem('token');
+            return Promise.reject();
+        }
+        return Promise.resolve();
+    },
+    getPermissions: params => Promise.resolve(),
 };
+
+ export default authProvider;
